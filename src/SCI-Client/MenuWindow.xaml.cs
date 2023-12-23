e17f7a534ffca65367b12b9486e541d6d3f991ca
Niko2405 <nilskornmann@gmail.com>
2023-12-24 00:36:15 +0100

    Create main menu

---
+++ src/SCI-Client/MenuWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SCI_Client
+{
+	/// <summary>
+	/// Interaction logic for MenuWindow.xaml
+	/// </summary>
+	public partial class MenuWindow : Window
+	{
+		public MenuWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
